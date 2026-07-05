4ae68300cd59d4c15cf1bef9afebc32bd3ed0f05
VedantChavan385 <vedantchavan385@gmail.com>
2026-07-05 21:39:43 +0530

    routing done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#e9e9e9]">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
