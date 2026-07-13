470477fd09d393aafc9a3a9bd8d2e873f30cbc2a
VedantChavan385 <vedantchavan385@gmail.com>
2026-07-13 19:13:46 +0530

    V3 intialization

---
+++ V3/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero'
+import About from './components/About'
+import Features from './components/Features'
+
+function App() {
+  return (
+    <div className="bg-black text-[#E1E0CC] min-h-screen w-full relative">
+      <Hero />
+      <About />
+      <Features />
+    </div>
+  )
+}
+
+export default App
