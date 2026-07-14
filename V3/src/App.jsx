e2ab418a09bf27cb94d0face530aa0ddb42e092e
VedantChavan385 <vedantchavan385@gmail.com>
2026-07-14 10:36:13 +0530

    Installed router

--- V3/src/App.jsx
+++ V3/src/App.jsx
@@ -1,15 +1,34 @@
-import Hero from './components/Hero'
-import About from './components/About'
-import Features from './components/Features'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
+import ProjectDetail from './pages/ProjectDetail';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="bg-black text-[#E1E0CC] min-h-screen w-full relative">
-      <Hero />
-      <About />
-      <Features />
-    </div>
-  )
+    <Router>
+      <div className="bg-black text-[#E1E0CC] min-h-screen w-full flex flex-col relative">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/work/:slug" element={<ProjectDetail />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
